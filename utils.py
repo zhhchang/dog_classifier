00805c340af23db0ae679b1c05a22040154f7137
Crayonkkk <maeecho@hotmail.com>
2021-11-18 20:36:32 +0800

    add back show_prob_dog_classifier()

--- utils.py
+++ utils.py
@@ -33,6 +33,60 @@ def show(X):
     else:
         print('WRONG TENSOR SIZE')
 
+
+def show_prob_dog_classifier(p):
+
+    p=p.data.squeeze().numpy()
+
+    ft=15
+    label = ('affenpinscher', 'beagle', 'boxer', 'chihuahua', 'frenchbulldog', 'goldenretriever', 'rottweiler', 'schnauzer', 'sheepdog','spaniel')
+    #p=p.data.squeeze().numpy()
+    y_pos = np.arange(len(p))*1.2
+    target=2
+    width=0.9
+    col= 'blue'
+    #col='darkgreen'
+
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+
+    # the plot
+    ax.barh(y_pos, p, width , align='center', color=col)
+
+    ax.set_xlim([0, 1.3])
+    #ax.set_ylim([-0.8, len(p)*1.2-1+0.8])
+
+    # y label
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(label, fontsize=ft)
+    ax.invert_yaxis()  
+    #ax.set_xlabel('Performance')
+    #ax.set_title('How fast do you want to go today?')
+
+    # x label
+    ax.set_xticklabels([])
+    ax.set_xticks([])
+    #x_pos=np.array([0, 0.25 , 0.5 , 0.75 , 1])
+    #ax.set_xticks(x_pos)
+    #ax.set_xticklabels( [0, 0.25 , 0.5 , 0.75 , 1] , fontsize=15)
+
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_linewidth(4)
+
+
+    for i in range(len(p)):
+        str_nb="{0:.2f}".format(p[i])
+        ax.text( p[i] + 0.05 , y_pos[i] ,str_nb ,
+                 horizontalalignment='left', verticalalignment='center',
+                 transform=ax.transData, color= col,fontsize=ft)
+
+
+
+    plt.show()
+
+
 def show_prob_cifar(p):
 
 
